15d1701abdeba857937ae7dbc835e400cdd7edc2
kotbi abderrahmane <kotbymo@gmail.com>
2021-06-07 22:28:42 +0100

    correct project structure.

--- src/com/ecomm/servelt/Accueil.java
+++ src/com/ecomm/servelt/Accueil.java
@@ -18,7 +18,7 @@ public class Accueil extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		this.getServletContext().getRequestDispatcher("/WEB-INF/accueil.jsp").forward(request, response);
+		this.getServletContext().getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
